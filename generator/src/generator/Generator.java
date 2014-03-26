c88a3a69a9c1315f105065d5e9bb8cda7f6b4b71
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-03-25 20:13:00 -0600

    Project as a netbeans project :P

---
+++ generator/src/generator/Generator.java
@@ -0,0 +1,15 @@
+package generator;
+
+/**
+ * @author Crisoforo Gaspar Hernández
+ */
+public class Generator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
